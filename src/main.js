652ed4c99bc4cb7b4afef8981cfdfc71482be81e
Juan José García <39605602+JuanJo193@users.noreply.github.com>
2019-08-16 20:11:58 -0500

    Juan García
    Se agregaron imports de los charts para los gráficos de la vista "Reportes".

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import "chart.js"
+import "hchs-vue-charts"
+Vue.use(window.VueCharts)
 
 Vue.config.productionTip = false
-
+Vue.component('grafica-component', require('./components/graficos.vue').default);
 new Vue({
     router,
     render: h => h(App)
