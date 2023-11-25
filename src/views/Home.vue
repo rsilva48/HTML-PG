e289bd63c8004caeca679f1bad11a1420346deff
Roger Silva <silvara99@outlook.com>
2023-11-25 11:52:35 -0500

    Changed Home Format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
     <div class="home">
         <div class="container-fluid px-4 py-5 px-md-5 font bg-light">
             <div class="container">
-                <h1 class="display-4">Biblioteca</h1>
-                <p class="lead">
-                    El Centro de Recursos para el Aprendizaje y la Investigación, CRAI, se concibe como un lugar que
-                    integra talento humano, libros físicos y espacios diseñado para estudiantes, dotado de tecnología
-                    que facilita el acceso a valiosos recursos digitales.
-                </p>
+                <h1 class="display-4">Biblioteca CRAI</h1>
+                <p class="lead">Haga clic en uno de los siguientes enlaces para utilizar nuestos servicios</p>
                 <hr class="my-4" />
-                <p class="lead">Haga clic en uno de los siguientes enlaces para utilizar nuestos servicios.</p>
                 <div class="d-grid gap-3 mx-auto d-md-block">
                     <router-link to="/cub" class="btn btn-primary btn-lg mx-1"> Cubículos </router-link>
                     <router-link to="/pc" class="btn btn-primary btn-lg mx-1"> Computadoras </router-link>
+                </div>
+                <div class="card my-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Acerca de</h5>
+                        <p class="card-text">
+                            El Centro de Recursos para el Aprendizaje y la Investigación, se concibe como un lugar que
+                            integra talento humano, libros físicos y espacios diseñado para estudiantes, dotado de
+                            tecnología que facilita el acceso a valiosos recursos digitales.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
