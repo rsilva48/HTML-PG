e99e7a7e763fb5b2617cd03e57378d09fb727bca
Roger Silva <silvara99@outlook.com>
2023-11-25 11:47:24 -0500

    Added Pinia + Updates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '../router'
 import './registerServiceWorker'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 import 'font-awesome/css/font-awesome.min.css'
 import 'chart.js'
 import './services/firebase'
@@ -11,6 +12,8 @@ import Vue3FormWizard from 'vue3-form-wizard'
 import 'vue3-form-wizard/dist/style.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(router)
 app.use(Vue3FormWizard)
+app.use(pinia)
 app.mount('#app')
