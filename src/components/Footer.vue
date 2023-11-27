e60145bb78b315c45cdc089010f5a58abc79d860
Roger Silva <silvara99@outlook.com>
2023-11-26 23:03:01 -0500

    Footer background and opacity

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,33 @@
 <template>
-    <div class="container fixed-bottom">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                    <i class="bi bi-code" width="30" height="24"></i>
-                </a>
-                <span class="mb-3 mb-md-0 text-body-secondary">2023 Roger Silva</span>
-            </div>
+    <div class="container-flex fixed-bottom bg-white" style="--bs-bg-opacity: 0.9">
+        <div class="container">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                        <i class="bi bi-code" width="30" height="24"></i>
+                    </a>
+                    <span class="mb-3 mb-md-0 text-body-secondary">2023 Roger Silva</span>
+                </div>
 
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li class="ms-3">
-                    <a class="text-body-secondary" href="https://github.com/rsilva48">
-                        <i class="bi bi-github" width="24" height="24"></i
-                    ></a>
-                </li>
-                <li class="ms-3">
-                    <a class="text-body-secondary" href="https://gitlab.com/silvara99">
-                        <i class="bi bi-gitlab" width="24" height="24"></i
-                    ></a>
-                </li>
-                <li class="ms-3">
-                    <a class="text-body-secondary" href="https://www.linkedin.com/in/rasilvab/">
-                        <i class="bi bi-linkedin" width="24" height="24"></i
-                    ></a>
-                </li>
-            </ul>
-        </footer>
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3">
+                        <a class="text-body-secondary" href="https://github.com/rsilva48">
+                            <i class="bi bi-github" width="24" height="24"></i
+                        ></a>
+                    </li>
+                    <li class="ms-3">
+                        <a class="text-body-secondary" href="https://gitlab.com/silvara99">
+                            <i class="bi bi-gitlab" width="24" height="24"></i
+                        ></a>
+                    </li>
+                    <li class="ms-3">
+                        <a class="text-body-secondary" href="https://www.linkedin.com/in/rasilvab/">
+                            <i class="bi bi-linkedin" width="24" height="24"></i
+                        ></a>
+                    </li>
+                </ul>
+            </footer>
+        </div>
     </div>
 </template>
 
